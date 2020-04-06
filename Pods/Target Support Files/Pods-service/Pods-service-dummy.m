da1aac9446b43c7f26ad86f46be27aa8dcd1f55b
Robert Dresler <robertdreslerjr@gmail.com>
2020-04-06 16:38:47 +0200

    Changed: Add pods to other modules

---
+++ Pods/Target Support Files/Pods-service/Pods-service-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_service : NSObject
+@end
+@implementation PodsDummy_Pods_service
+@end
---
+++ service/service/service.h
@@ -0,0 +1,19 @@
+//
+//  service.h
+//  service
+//
+//  Created by Robert Dresler on 06/04/2020.
+//  Copyright © 2020 Robert Dresler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for service.
+FOUNDATION_EXPORT double serviceVersionNumber;
+
+//! Project version string for service.
+FOUNDATION_EXPORT const unsigned char serviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <service/PublicHeader.h>
+
+
