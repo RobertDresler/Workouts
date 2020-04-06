f35e731a65f54231b84a8c5a79148ef1e9f5e0ab
Robert Dresler <robertdreslerjr@gmail.com>
2020-04-06 16:42:29 +0200

    Changed: Create WorkoutsUI module

---
+++ WorkoutsUI/WorkoutsUI/WorkoutsUI.h
@@ -0,0 +1,19 @@
+//
+//  WorkoutsUI.h
+//  WorkoutsUI
+//
+//  Created by Robert Dresler on 06/04/2020.
+//  Copyright © 2020 Robert Dresler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkoutsUI.
+FOUNDATION_EXPORT double WorkoutsUIVersionNumber;
+
+//! Project version string for WorkoutsUI.
+FOUNDATION_EXPORT const unsigned char WorkoutsUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkoutsUI/PublicHeader.h>
+
+
